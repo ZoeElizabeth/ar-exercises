1d889bd47064f48a7a5193bf8e11c205ce994bca
Zoe Gelech <zoe.gelech@gmail.com>
2018-11-06 22:18:37 +0000

    New solution for step 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -19,12 +19,19 @@ puts "----------"
 # 4. Do another fetch but this time load stores that carry women's apparel and are generating less than $1M in annual revenue.
 
 
-surrey = Store.create name: 'Surrey', annual_revenue: '224000', mens_apparel: 'false', womens_apparel: 'true'
-whistler = Store.create name: 'Whistler', annual_revenue: '1900000', mens_apparel: 'true', womens_apparel: 'false'
-yaletown = Store.create name: 'Yaletown', annual_revenue: '430000', mens_apparel: 'true', womens_apparel: 'true'
+surrey = Store.create name: 'Surrey', annual_revenue: 224000, mens_apparel: 'false', womens_apparel: 'true'
+whistler = Store.create name: 'Whistler', annual_revenue: 1900000, mens_apparel: 'true', womens_apparel: 'false'
+yaletown = Store.create name: 'Yaletown', annual_revenue: 430000, mens_apparel: 'true', womens_apparel: 'true'
 
 @mens_stores = Store.where(mens_apparel: 'true')
 
-@mens_stores.each do |store|
-    p "#{store.name} makes #{store.annual_revenue} annually"
-  end
+# @mens_stores.each do |store|
+#     p "#{store.name} makes #{store.annual_revenue} annually"
+# end
+
+# @womens_stores = Store.where(womens_apparel: 'true').where("annual_revenue >= ?", 1000000)
+@womens_stores = Store.where(womens_apparel: 'true', annual_revenue: 1000000..Float::INFINITY)
+
+@womens_stores.each do |store|
+  p "#{store.name} makes #{store.annual_revenue} annually"
+end
